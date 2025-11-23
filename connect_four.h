e1dec6d1ceb4a8af5662a377a38423d08f30a76f
BabuFrick007 <mariane.ch@gmail.com>
2025-11-23 16:07:15 +0100

    Create connect_four.h : Version 1

---
+++ connect_four.h
@@ -0,0 +1,26 @@
+#ifndef CONNECT_FOUR_H
+#define CONNECT_FOUR_H
+
+// Standard Connect Four board size
+#define ROWS 6
+#define COLS 7
+
+// Cell states / tokens
+#define EMPTY   '.'
+#define PLAYER1 'X'
+#define PLAYER2 'O'
+
+// -------- Core game logic (connect_four.c) --------
+void initializeBoard(char board[ROWS][COLS]);
+int  dropPiece(char board[ROWS][COLS], int col, char piece);
+int  checkWin(char board[ROWS][COLS], char piece, int last_row, int last_col);
+int  isBoardFull(char board[ROWS][COLS]);
+
+// -------- I/O + display + CPU (io_engine.c) --------
+void displayBoard(char board[ROWS][COLS]);
+int  getHumanMove(char board[ROWS][COLS], char piece);
+int  getCPUMove(char board[ROWS][COLS], char piece);
+int  isMoveValid(char board[ROWS][COLS], int col);
+int  selectGameMode(void);
+
+#endif
